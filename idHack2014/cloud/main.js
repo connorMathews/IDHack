b280398b54e667f9248f0f90d1c456169a6fb9f9
Jonathan Kolnik <kolnik.com@gmail.com>
2014-02-15 23:36:14 -0500

    working sms

--- idHack2014/cloud/main.js
+++ idHack2014/cloud/main.js
@@ -1,31 +1,5 @@
 // Include Cloud Code module dependencies
-var express = require('express'),
-    twilio = require('twilio');
-
-// Create an Express web app (more info: http://expressjs.com/)
-var app = express();
-
-// Create a route that will respond to am HTTP GET request with some
-// simple TwiML instructions
-app.get('/hello', function(request, response) {
-
-    message_body = request.values.get('Body', None)
-    if message_body:
-        information = message_body.split('/')
-        console.log(information)
-
-    // Create a TwiML response generator object
-    var twiml = new twilio.TwimlResponse();
-
-    // add some instructions
-    twiml.say('Hello there! Isn\'t Parse cool?', {
-        voice: 'woman'
-    });
-
-    // Render the TwiML XML document
-    response.type('text/xml');
-    response.send(twiml.toString());
+Parse.Cloud.define("receiveSMS", function(request, response) {
+    console.log("Received a new text: " + request.params.Body);
+    response.success();
 });
-
-// Start the Express app
-app.listen();

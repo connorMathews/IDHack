d0d54778b76f965748b65656571275b3fb14edf6
KrisNi <krisnii.com@gmail.com>
2014-02-15 23:59:22 -0500

    add parsing message

--- idHack2014/cloud/main.js
+++ idHack2014/cloud/main.js
@@ -1,5 +1,24 @@
 // Include Cloud Code module dependencies
 Parse.Cloud.define("receiveSMS", function(request, response) {
+
     console.log("Received a new text: " + request.params.Body);
+
+    var Reports = Parse.Object.extend("Reports");
+    var reports = new Reports();
+    var information = request.params.Body.split("/");
+
+    reports.set("phoneNum", request.params.From);
+    reports.set("headline", information[1]);
+    reports.set("comments", information[2]);
+    reports.set("severity", information[3]);
+
+    reports.save(null, {
+      success: function(reports) {
+        alert('New object created with objectId: ' + reports.id);
+      },
+      error: function(reports, error) {
+        alert('Failed to create new object, with error code: ' + error.description);
+      }
+    });
     response.success();
 });

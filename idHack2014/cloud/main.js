0a56e4c42a18a7a2209686bd2f5cbd68d6c7028d
Jonathan Kolnik <kolnik.com@gmail.com>
2014-02-16 00:00:11 -0500

    changes

--- idHack2014/cloud/main.js
+++ idHack2014/cloud/main.js
@@ -1,5 +1,8 @@
 // Include Cloud Code module dependencies
 Parse.Cloud.define("receiveSMS", function(request, response) {
     console.log("Received a new text: " + request.params.Body);
+
+
+
     response.success();
 });

1e2bfc272ab72845a7d5d52a94ef727766f44c69
Jonathan Kolnik <kolnik.com@gmail.com>
2014-02-15 22:51:38 -0500

    twilio

--- idHack2014/cloud/main.js
+++ idHack2014/cloud/main.js
@@ -1,8 +1,25 @@
+// Include Cloud Code module dependencies
+var express = require('express'),
+    twilio = require('twilio');
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+// Create an Express web app (more info: http://expressjs.com/)
+var app = express();
+
+// Create a route that will respond to am HTTP GET request with some
+// simple TwiML instructions
+app.get('/hello', function(request, response) {
+    // Create a TwiML response generator object
+    var twiml = new twilio.TwimlResponse();
+
+    // add some instructions
+    twiml.say('Hello there! Isn\'t Parse cool?', {
+        voice: 'woman'
+    });
+
+    // Render the TwiML XML document
+    response.type('text/xml');
+    response.send(twiml.toString());
 });
 
-
+// Start the Express app
+app.listen();
